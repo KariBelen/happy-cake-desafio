704056ab6b1879c71c3f7a2009cdf8bd08a0380c
karibelen <karinabeleengonzalez@gmail.com>
2022-09-25 02:54:00 -0300

    Se sube proyecto

---
+++ src/Views/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container pt-4">
+      <h1>
+        Bienvenido a <span className="fw-bold">Happy Cake</span>
+      </h1>
+      <h6>El lugar de los pasteles felices</h6>
+      <span className="pastel">🎂</span>
+    </div>
+  );
+};
+
+export default Home;
